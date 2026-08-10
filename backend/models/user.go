069c3e584f9dea70104f72330288496b875d2f0e
Ashomondi <adhiamboa745@gmail.com>
2026-08-10 15:55:15 +0300

    fixed the indefined struct

--- backend/models/user.go
+++ backend/models/user.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type User struct {
-	ID  string  `json""id"`
+	ID  string  `json:"id"`
 	Email  string  `json:"email"`
 	Password  string  `json:"-"`
-	CreatedAt  time.time  `json:"created_at"`
-	UpdatedAt  time.time `json:"updated_at"`
+	CreatedAt  time.Time  `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
 }
